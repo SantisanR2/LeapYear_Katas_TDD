a2a9f5909baef42487747101b33962710c4a8a04
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-10-16 20:06:08 -0500

    🟥 Debe_Dar_True_Si_Es_Divisible_4

--- LeapYearTest.cs
+++ LeapYearTest.cs
@@ -1,9 +1,25 @@
-﻿namespace LeapYear;
+﻿using AwesomeAssertions;
+
+namespace LeapYear;
 
 public class LeapYearTest
 {
     [Fact]
     public void Test1()
     {
+        //Arrange
+        var year = 16;
+
+        //Act
+        var isLeapYear = IsLeapYear(year);
+
+        //Assert
+        isLeapYear.Should().Be(true);
+
+    }
+
+    private bool IsLeapYear(int year)
+    {
+        throw new NotImplementedException();
     }
 }
